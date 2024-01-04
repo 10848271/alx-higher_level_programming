271270ea1eb097d5fec879472106a04994ef958c
Kingsley Acquah <kingsley.acquah@amalitech.com>
2024-01-04 00:55:55 +0000

    correct errors

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,11 +8,11 @@ else:
     last_digit = number % 10
 
 response_format = "Last digit of {} is {} {}"
+response_suffix3 = "and is less than 6 and not 0"
 
 if last_digit == 0:
-    print(response_format.format(number, last_digit, "zero"))
+    print(response_format.format(number, last_digit, "0"))
 elif last_digit > 5:
     print(response_format.format(number, last_digit, "and is greater than 5"))
 else:
-    print(response_format.format(number, last_digit, "and is less than 6 and not 0"))
-
+    print(response_format.format(number, last_digit, response_suffix3))
